caa87a7332f1840afe7ad8a76a08e2c5ec2fcffd
amit <amitsarker95@gmail.com>
2024-03-04 02:31:23 +0600

    Products admin upgraded

--- store/admin.py
+++ store/admin.py
@@ -5,6 +5,7 @@ from .models import Product, ProductCategory, ProductInventory, Cart, CartItem
 
 admin.site.register(ProductCategory)
 admin.site.register(ProductInventory)
+# admin.site.register(Product)
 
 
 
@@ -24,46 +25,51 @@ class InventoryFilter(admin.SimpleListFilter):
     def queryset(self, request, queryset: QuerySet):
         if self.value() == self.low_stock:
             return queryset.filter(inventory__lt=10)
-        return queryset.filter(inventory__gt=10)
-    
+        elif self.value() == self.full_stock:
+            return queryset.filter(inventory__gte=10) 
+        else:
+            return queryset 
 
-@admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     search_fields = ['name__istartswith']
-    actions = ['clear_inventory']
-    list_display = ['name', 'price', 'inventory_status', 'category']
+    list_display = ['name', 'price', 'category', 'inventory_status', 'product_count']
     list_filter = ['category', 'modified_at', InventoryFilter]
 
     @admin.display(ordering='inventory')
     def inventory_status(self, product):
-        if product.inventory < 10:
+        if product.inventory.quantity < 10:
             return 'Low'
-        return 'Full'
-    
+        else:
+            return 'Full'
+    @admin.display(ordering='quantity')
+    def product_count(self, product):
+        total = product.inventory.quantity
+        return f'{total}'
 
     @admin.action(description='Clear inventory')
     def clear_inventory(self, request, queryset):
         updated_count = queryset.update(inventory=0)
         self.message_user(
             request,
-            f"{updated_count} product's were successfully updated.",
-            messages.SUCCESS
+            f"{updated_count} product(s) were successfully updated.",
+            level=admin.messages.SUCCESS
         )
+admin.site.register(Product, ProductAdmin)
 
 
-    @admin.register(Cart)
-    class CartAdmin(admin.ModelAdmin):
-        list_display = ['id', 'created']
-        search_fields = ['id']
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ['id', 'created']
+    search_fields = ['id']
 
 
-    @admin.register(CartItem)
-    class CartItemAdmin(admin.ModelAdmin):
+@admin.register(CartItem)
+class CartItemAdmin(admin.ModelAdmin):
 
-        search_fields = ['id']
-        list_display = ['id', 'cart', 'product', 'total_price']
-        def total_price(self, cart_item:CartItem):
-            return cart_item.quantity * cart_item.product.price
-        total_price.short_description = 'Total Price'
+    search_fields = ['id']
+    list_display = ['id', 'cart', 'product', 'total_price']
+    def total_price(self, cart_item:CartItem):
+        return cart_item.quantity * cart_item.product.price
+    total_price.short_description = 'Total Price'
 
 
